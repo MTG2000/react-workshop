0d64b58ea076ac3a5a2ec0a5a547957d7a94fe4a
MTG2000 <mtg0987654321@gmail.com>
2022-02-28 11:30:05 +0200

    commit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import Buttons from "./Buttons";
 
 export default function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  const handleResetClick = () => {
+    setCounter(0);
+  };
+
+  const handleIncrementClick = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleDecrementClick = () => {
+    setCounter(counter - 1);
+  };
+
+  const valueToShow = counter < 10 ? "0" + counter : counter;
+
   return (
     <div className="container">
-      <h2 className="value">00</h2>
+      <h2 className="value">{valueToShow}</h2>
       <div className="btns">
-        <button>Reset</button>
-        <button>Decrement</button>
-        <button>Increment</button>
+        <Buttons
+          handleDecrementClick={handleDecrementClick}
+          handleIncrementClick={handleIncrementClick}
+          handleResetClick={handleResetClick}
+        />
       </div>
     </div>
   );
