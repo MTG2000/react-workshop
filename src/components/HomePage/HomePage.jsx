2152addbf97d562ba08007ae849f2a96042b2f56
MTG2000 <mtg0987654321@gmail.com>
2022-03-02 21:47:44 +0200

    improve home screen

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -4,6 +4,7 @@ import "./homepage-styles.css";
 export default function HomePage() {
   return (
     <div className="home-page">
+      <img src="logo512.png" alt="React logo" />
       <h1>React Workshop Projects</h1>
     </div>
   );
